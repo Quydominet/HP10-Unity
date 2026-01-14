2a76f3ad71771bc163d6ad0dcdbfbf561effaa45
snltwphong <snltwphong@gmail.com>
2026-01-14 18:45:22 +0700

    update UI MainMenu

---
+++ Assets/Scripts/ButtonHoverEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Vector3 originalScale;
+    public float scaleFactor = 1.2f; // Độ phóng to
+    public float speed = 0.1f;      // Tốc độ biến đổi
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
+    // Khi chuột rê vào
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StopAllCoroutines(); // Dừng các hiệu ứng cũ nếu có
+        transform.localScale = originalScale * scaleFactor;
+        transform.SetAsLastSibling();
+    }
+
+    // Khi chuột rời khỏi
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = originalScale;
+    }
+}
