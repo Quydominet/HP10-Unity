fbd82ced79c0631424275c520fdaa08c655d360f
Benedict1210 <icydeek@gmail.com>
2026-05-06 19:50:32 +0700

    Game Saving and Encryption

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    SaveHandler IngameSave = new SaveHandler();
+
+    private static readonly Dictionary<string, int> Defaults = new Dictionary<string, int>
+    {
+        ["Kills"] = 100,
+        ["Level"] = 1,
+        ["Exp"] = 0,
+
+        ["Unlock0"] = 0, // Sedan -> 0, Muscle Car, F1 -> 1, Truck -> 2
+        ["Unlock1"] = 0, // Brownings -> 0, Gatlings -> 1, Miniguns -> 2
+        ["Unlock2"] = 0, // 50 Cal -> 0, 20mm -> 1, 40mm -> 2
+        ["Unlock3"] = 0, // Armor Piercing -> 0, Explosive -> 1, Incendiary -> 2
+        ["Unlock4"] = 0, // None -> 0, More HP -> 1, More HP -> 2
+        ["Unlock5"] = 0, // None -> 0, Nitrous Upgrade -> 1, Acceleration Increase -> 2
+
+        ["Slot0"] = 0, // Car
+        ["Slot1"] = 0, // Gun
+        ["Slot2"] = 0, // Ammo
+        ["Slot3"] = 0, // Type
+    };
+    void Awake()
+    {
+        IngameSave.LoadData();
+
+        foreach (var entry in Defaults)
+        {
+            if (!IngameSave.data.ContainsKey(entry.Key))
+                IngameSave.data[entry.Key] = entry.Value;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        IngameSave.SaveData();
+    }
+}
---
+++ Assets/Scripts/GameSaveHandler.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System;
+using System.Collections.Generic;
+
+//this script saves and loads all the info we want
+public class SaveHandler : MonoBehaviour
+{
+    string path = "Player.save";
+
+    //data is what is finally saved
+    public Dictionary<string, int> data;
+
+    void Awake()
+    {
+        LoadData();
+        //WARNING! data.Clear() deletes EVERYTHING
+        //data.Clear();
+        //SaveData();
+    }
+
+    public void LoadData()
+    {
+        data = SaveHandler.DeserializeData<Dictionary<string, int>>(path);
+    }
+
+    public void SaveData() => SaveHandler.SerializeData(data, path);
+    public static void SerializeData<T>(T data, string path)
+    {
+        FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+        BinaryFormatter formatter = new BinaryFormatter();
+        try
+        {
+            formatter.Serialize(fs, data);
+            Debug.Log("Data written to " + path + " @ " + DateTime.Now.ToShortTimeString());
+        }
+        catch (SerializationException e)
+        {
+            Debug.LogError(e.Message);
+        }
+        finally
+        {
+            fs.Close();
+        }
+    }
+
+    public static T DeserializeData<T>(string path)
+    {
+        T data = default(T);
+
+        if (File.Exists(path))
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                data = (T)formatter.Deserialize(fs);
+                Debug.Log("Data read from " + path);
+            }
+            catch (SerializationException e)
+            {
+                Debug.LogError(e.Message);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        return data;
+    }
+}
