21b08c9e1e42206bc8603755cad3b7297968c9ce
snltwphong <snltwphong@gmail.com>
2026-07-15 18:38:59 +0700

    lasst
    lasst

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Thư viện quản lý Scene
+
+public class ChangeScene : MonoBehaviour
+{
+    void Update()
+    {
+        // Kiểm tra nếu người dùng bấm phím Enter
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            LoadNextSceneIndex();
+        }
+    }
+
+    void LoadNextSceneIndex()
+    {
+        // Lấy chỉ số index của Scene hiện tại
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Cộng thêm 1 để lấy index của Scene tiếp theo
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Kiểm tra xem có Scene tiếp theo trong danh sách không, tránh bị lỗi vượt quá số lượng Scene
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("Đây đã là Scene cuối cùng trong danh sách Build Profiles rồi!");
+        }
+    }
+}
---
+++ Assets/Scripts/ExitGames.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ExitGames : MonoBehaviour
+{
+
+    public void ExitGame()
+    {
+        Debug.Log("Game đang thoát...");
+        Application.Quit();
+
+        #if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
